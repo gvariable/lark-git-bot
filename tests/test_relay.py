a316e4173d225bc385d470bd43b1e9e753dd6ddf
gpl <gplhust@hust.edu.cn>
2023-07-01 17:11:46 +0800

    test: Add relay test case

---
+++ tests/test_relay.py
@@ -0,0 +1,41 @@
+import unittest
+import requests
+import json
+
+
+class TestRelay(unittest.IsolatedAsyncioTestCase):
+    def setUp(self) -> None:
+        self.host = "localhost"
+        self.port = 8080
+
+    def send(self, headers, payload):
+        return requests.post(
+            f"http://{self.host}:{self.port}/relay",
+            headers=headers,
+            json=payload,
+        )
+
+    def test_push(self):
+        with open("tests/data/push.json", "r") as f:
+            headers = {"X-GitHub-Event": "push"}
+            payload = json.load(f)
+            resp = self.send(headers, payload)
+            self.assertEqual(resp.status_code, 200)
+
+    def test_pr(self):
+        with open("tests/data/pr.json", "r") as f:
+            headers = {"X-GitHub-Event": "pull_request"}
+            payload = json.load(f)
+            resp = self.send(headers, payload)
+            self.assertEqual(resp.status_code, 200)
+
+    def test_issue(self):
+        with open("tests/data/issues.json", "r") as f:
+            headers = {"X-GitHub-Event": "issues"}
+            payload = json.load(f)
+            resp = self.send(headers, payload)
+            self.assertEqual(resp.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
