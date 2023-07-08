4bf98e29cdd6b0f65efa5bbf7997d2f2247b5d81
gpl <gplhust@hust.edu.cn>
2023-07-08 22:46:20 +0800

    chore: Refactor code to use env for secret loading

--- bot.py
+++ bot.py
@@ -3,36 +3,34 @@ from jinja2 import Environment, FileSystemLoader
 import hashlib
 import base64
 import hmac
-from dotenv import dotenv_values
 import json
 import aiohttp
 
 
-CONFIG = dotenv_values(".env")
 env = Environment(loader=FileSystemLoader("./"))
 template = env.get_template("template.json")
 
 
-def gen_sign(timestamp):
-    s = f"{timestamp}\n{CONFIG['SECRET']}"
+def gen_sign(secret, timestamp):
+    s = f"{timestamp}\n{secret}"
     hmac_code = hmac.new(s.encode("utf-8"), digestmod=hashlib.sha256).digest()
     sign = base64.b64encode(hmac_code).decode("utf-8")
     return sign
 
 
-async def send_card(meta):
-    url = CONFIG["WEBHOOK"]
+async def send_card(meta, webhook, secret):
+    # TODO(gpl): handle another case if secret is not provided
     timestamp = int(time.time())
-    sign = gen_sign(timestamp)
+    sign = gen_sign(timestamp, secret)
 
     data = {
         "timestamp": str(timestamp),
         "sign": sign,
         "msg_type": "interactive",
-        "card": json.loads(template.render(meta=meta),strict=False),
+        "card": json.loads(template.render(meta=meta), strict=False),
     }
     async with aiohttp.ClientSession() as session:
-        async with session.post(url=url, json=data) as resp:
+        async with session.post(url=webhook, json=data) as resp:
             data = await resp.json()
             if data["code"] != 0:
                 raise Exception(data["msg"])
--- relay.py
+++ relay.py
@@ -1,6 +1,8 @@
 from aiohttp import web
 from parse import *
 from bot import send_card
+import argparse
+import os
 
 
 async def relay(request: web.Request):
@@ -13,11 +15,22 @@ async def relay(request: web.Request):
         case "pull_request" | "issues":
             meta = InteractivePayload(event, data)
     print(meta)
-    await send_card(meta)
+    await send_card(meta, os.environ.get("WEBHOOK"), os.environ.get("SECRET"))
     return web.Response(text="OK")
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    args = parser.parse_args()
+
+    if os.environ.get("WEBHOOK") is None:
+        from dotenv import load_dotenv
+
+        if not load_dotenv(".env"):
+            # TODO(gpl): error handling
+            ...
+
     app = web.Application()
     app.add_routes([web.post("/relay", relay)])
-    web.run_app(app, host="0.0.0.0", port=8080)
+    web.run_app(app, host="0.0.0.0", port=args.port)
